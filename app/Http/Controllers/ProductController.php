c03602e6bd6063a338888b33a8900735ab772b57
Juan Manuel Cruz <cruz.jm.stafe@gmail.com>
2021-03-16 19:01:40 -0300

    order by code

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -14,7 +14,7 @@ class ProductController extends Controller
      */
     public function index()
     {
-        return Product::with('supplier', 'categories')->get();
+        return Product::with('supplier', 'categories')->orderBy('code')->get();
     }
 
     /**
