8553c01c0373368b1dbc5022a1ca4a078238c74f
Juan Manuel Cruz <cruz.jm.stafe@gmail.com>
2026-01-30 15:40:06 -0300

    refactor function to avoid complexity and hide hashing in the model method

--- app/Models/User.php
+++ app/Models/User.php
@@ -8,6 +8,7 @@ use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Relations\HasMany;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
+use Illuminate\Support\Facades\Hash;
 use Laravel\Passport\HasApiTokens;
 
 class User extends Authenticatable
@@ -96,6 +97,16 @@ class User extends Authenticatable
     {
         return Attribute::make(
             get: fn () => $this->role === 'admin',
+        );
+    }
+
+    /**
+     * Set the user's password.
+     */
+    protected function password(): Attribute
+    {
+        return Attribute::make(
+            set: fn (string $value) => ['password' => Hash::make($value)],
         );
     }
 }
