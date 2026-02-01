e20b59991c4915914f79dd0e9ca12c30ffd1bee8
Juan Manuel Cruz <cruz.jm.stafe@gmail.com>
2026-01-31 23:47:36 -0300

    add tests

--- app/Http/Controllers/BrandController.php
+++ app/Http/Controllers/BrandController.php
@@ -24,7 +24,7 @@ class BrandController extends Controller
     /**
      * Store a newly created resource in storage.
      */
-    public function store(StoreRequest $request)
+    public function store(StoreRequest $request): Brand
     {
         $user = $request->user('api');
 
@@ -38,7 +38,7 @@ class BrandController extends Controller
     /**
      * Display the specified resource.
      */
-    public function show(Brand $brand)
+    public function show(Brand $brand): Brand
     {
         $this->authorize('view', $brand);
 
@@ -50,7 +50,7 @@ class BrandController extends Controller
     /**
      * Update the specified resource in storage.
      */
-    public function update(UpdateRequest $request, Brand $brand)
+    public function update(UpdateRequest $request, Brand $brand): Brand
     {
         $brand->fill($request->validated())->save();
 
@@ -60,13 +60,12 @@ class BrandController extends Controller
     /**
      * Remove the specified resource from storage.
      */
-    public function destroy(Brand $brand)
+    public function destroy(Brand $brand): Brand
     {
         $this->authorize('delete', $brand);
 
         $brand->delete();
 
         return $brand;
-
     }
 }
