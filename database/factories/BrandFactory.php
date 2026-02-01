668206600d6b989110b1430abac64b11069aea40
Milton Wery <milton_st@hotmail.com>
2026-01-31 21:14:39 -0300

    Added brand model migrations

---
+++ database/factories/BrandFactory.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace Database\Factories;
+
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Brand>
+ */
+class BrandFactory extends Factory
+{
+   /**
+     * The name of the factory's corresponding model.
+     *
+     * @var string
+     */
+    protected $model = Brand::class;
+
+    /**
+     * Define the model's default state.
+     *
+     * @return array
+     */
+    public function definition()
+    {
+        return [
+            'uuid' => Str::uuid(),
+            'name' => $this->faker->name,
+            'user_id' => User::factory(),
+        ];
+    }
+}
